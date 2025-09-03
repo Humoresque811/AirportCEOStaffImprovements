a6628f8fa25ad30da0e8347ee2f6582cc39b4643
iSamity <gamandsoc@gmail.com>
2025-09-03 23:05:30 +0200

    Add patches to regenerate new applications immediately

---
+++ ImmediatelyAddApplication/FireEmployeePatch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+namespace AirportCEOStaffImprovements.ImmediatelyAddApplication;
+
+[HarmonyPatch(typeof(CandidateController), nameof(CandidateController.RejectEmployee))]
+internal class FireEmployeePatch
+{
+    [HarmonyPostfix]
+    static void PostfixPatch()
+    {
+        var instance = Singleton<CandidateController>.instance;
+
+        instance.StartCoroutine(instance.ValidateApplications());
+    }
+}
---
+++ ImmediatelyAddApplication/HireEmployeePatch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+
+namespace AirportCEOStaffImprovements.ImmediatelyAddApplication;
+
+[HarmonyPatch(typeof(CandidateController), nameof(CandidateController.HireEmployee))]
+internal class HireEmployeePatch
+{
+    [HarmonyPostfix]
+    static void PostfixPatch(ref bool __result)
+    {
+        if (!__result)
+        {
+            return;
+        }
+
+        var instance = Singleton<CandidateController>.instance;
+
+        instance.StartCoroutine(instance.ValidateApplications());
+    }
+}
