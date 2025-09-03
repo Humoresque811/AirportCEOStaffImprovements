8678b8ccbfa027b25ee44a169039124e176b99f4
Humoresque <humoresqueceo@gmail.com>
2025-09-03 16:41:50 -0700

    Mostly working new staff hiring options
    Consolidated files as well, added whole new system for quickly adding staff to the list

---
+++ ImmediatelyAddApplication/FasterEmployeeRespawnManager.cs
@@ -0,0 +1,89 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+using static Enums.EmployeeType;
+
+namespace AirportCEOStaffImprovements.ImmediatelyAddApplication;
+
+[HarmonyPatch(typeof(CandidateController))]
+internal class FasterEmployeeRespawnManager
+{
+    internal static void GenerateEmployee(Enums.EmployeeType employeeType)
+    {
+        int maxNumberOfEmployeesToGenerate = employeeType switch
+        {
+            CIO or HRDirector or ProcurementDirector or StrategyDirector or CFO or COO => 2,
+            _ => SIConfig.MaxStaffGenerated.Value,
+        };
+
+        bool hasCommercialLicense = Singleton<ProgressionManager>.Instance.GetProjectCompletionStatus(Enums.SpecificProjectType.CommercialLicense);
+        bool shouldGenerateEmployee;
+        if (maxNumberOfEmployeesToGenerate == 2)
+        {
+            shouldGenerateEmployee = true; // this is a way to catch all the admins
+        }
+        else
+        {
+            shouldGenerateEmployee = employeeType switch
+            {
+                PassengerServiceAgent => hasCommercialLicense && CandidateController.Instance.generateAirportStaff,
+                SecurityOfficer => hasCommercialLicense && CandidateController.Instance.generateSecurity,
+                RampAgent => hasCommercialLicense && CandidateController.Instance.generateRampAgent,
+                Janitor => CandidateController.Instance.generateJanitor,
+                ServiceTechnician => CandidateController.Instance.generateServiceTechnician,
+                Admin => CandidateController.Instance.generateAdmin,
+                _ => true,
+            };
+        }
+
+        if (!shouldGenerateEmployee)
+        {
+            return;
+        }
+
+        System.Random rand = new System.Random();
+        for (int i = 0; i < maxNumberOfEmployeesToGenerate - NumberOfPotentialEmployees(employeeType); i++)
+        {
+            TimeSpan delay = new TimeSpan(0, 0, 0, 0, Mathf.RoundToInt(SIConfig.StaffRespawnSpeed.Value * 1000) + rand.Next(-100, 500)); // Add preset time plus an extra -0.1 to 0.5 seconds
+            CandidateController.Instance.StartCoroutine(GenerateEmployeeDelayed(employeeType, maxNumberOfEmployeesToGenerate, delay));
+        }
+    }
+
+    private static IEnumerator GenerateEmployeeDelayed(Enums.EmployeeType employeeType, int maxNumberOfEmployeesToGenerate, TimeSpan delay)
+    {
+        yield return new WaitForSeconds(delay.Milliseconds / 1000f);
+
+        if (NumberOfPotentialEmployees(employeeType) < maxNumberOfEmployeesToGenerate)
+        {
+            CandidateController.Instance.GenerateCandidate(employeeType);
+            CandidateController.Instance.Refresh(true);
+        }
+    }
+
+    private static int NumberOfPotentialEmployees(Enums.EmployeeType employeeType)
+    {
+        return Singleton<AirportController>.Instance.allEmployees.Where(emp => emp.EmployeeType == employeeType && !emp.employeeModel.isHired && !emp.employeeModel.isFired).Count();
+
+    }
+
+    [HarmonyPatch(nameof(CandidateController.RejectEmployee))]
+    [HarmonyPostfix]
+    internal static void FireEmployeePatch(CandidateController __instance, EmployeeController employee)
+    {
+        GenerateEmployee(employee.EmployeeType);
+    }
+
+    [HarmonyPatch(nameof(CandidateController.HireEmployee))]
+    [HarmonyPostfix]
+    internal static void HireEmployeePatch(ref bool __result, CandidateController __instance, EmployeeController employee)
+    {
+        if (!__result)
+        {
+            return;
+        }
+
+        GenerateEmployee(employee.EmployeeType);
+    }
+}
--- SIConfig.cs
+++ SIConfig.cs
@@ -10,9 +10,15 @@ namespace AirportCEOStaffImprovements;
 internal class SIConfig
 {
     internal static ConfigEntry<bool> UseTrainAllButton { get; private set; }
+    internal static ConfigEntry<float> StaffRespawnSpeed { get; private set; }
+    internal static ConfigEntry<int> MaxStaffGenerated { get; private set; }
 
     internal static void SetUpConfig()
     {
         UseTrainAllButton = AirportCEOStaffImprovements.ConfigReference.Bind("General", "Create Train All Button", true, "Create a button to train all staff with.");
+        StaffRespawnSpeed = AirportCEOStaffImprovements.ConfigReference.Bind("General", "Staff Respawn Speed", 1f, 
+            new ConfigDescription("Around how fast should staff respawn?", new AcceptableValueRange<float>(0.5f, 5f)));
+        MaxStaffGenerated = AirportCEOStaffImprovements.ConfigReference.Bind("General", "Max Staff Generated", 8, 
+            new ConfigDescription("The max number of staff generated for hiring at a given time, per type", new AcceptableValueRange<int>(8, 16)));
     }
 }
