23b9289a59dba0ed64020ea76400c17af928a7f6
iSamity <gamandsoc@gmail.com>
2025-09-04 16:02:48 +0200

    add refresh on change settings using patch on InitializePanel

---
+++ SortingEmployees/RefreshPanelOnSettingChangedPatch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+
+namespace AirportCEOStaffImprovements.SortingEmployees;
+
+[HarmonyPatch(typeof(EmployeePanelUI), nameof(EmployeePanelUI.InitializePanel))]
+internal class RefreshPanelOnSettingChangedPatch
+{
+    [HarmonyPostfix]
+    static void PostfixPatch(EmployeePanelUI __instance)
+    {
+        SIConfig.SortOptions.SettingChanged += (s, e) =>
+        {
+            __instance.GenerateEmployeeContainers();
+        };
+        SIConfig.SortDirection.SettingChanged += (s, e) =>
+        {
+            __instance.GenerateEmployeeContainers();
+        };
+    }   
+}
