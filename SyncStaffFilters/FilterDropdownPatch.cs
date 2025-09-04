0364e12b001405c91ddb11987eb0eb2856a32574
iSamity <gamandsoc@gmail.com>
2025-09-04 09:59:58 +0200

    Sync dropdowns of staff and applicant

---
+++ SyncStaffFilters/FilterDropdownPatch.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+
+namespace AirportCEOStaffImprovements.SyncStaffFilters;
+
+[HarmonyPatch(typeof(EmployeePanelUI))]
+internal class FilterDropdownPatch
+{
+    [HarmonyPatch(nameof(EmployeePanelUI.FilterCategory), MethodType.Setter)]
+    [HarmonyPostfix]
+    static void SetFilterCategoryPatch(EmployeePanelUI __instance)
+    {
+        var currentValue = __instance.FilterCategory;
+
+        __instance.employeeSearchCategory = currentValue;
+        __instance.applicantSearchCategory = currentValue;
+    }
+
+    [HarmonyPatch(nameof(EmployeePanelUI.FilterValue), MethodType.Setter)]
+    [HarmonyPostfix]
+    static void SetFilterValuePatch(EmployeePanelUI __instance)
+    {
+        var currentValue = __instance.FilterValue;
+
+        __instance.employeeFilterValue = currentValue;
+        __instance.applicantFilterValue = currentValue;
+    }
+}
