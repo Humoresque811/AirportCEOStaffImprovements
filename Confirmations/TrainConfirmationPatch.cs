6715daf0c9ed8095b34ffb3311598cce4502d4b3
iSamity <gamandsoc@gmail.com>
2025-09-10 12:49:30 +0200

    add confirmation options with default true for fire and reject

---
+++ Confirmations/TrainConfirmationPatch.cs
@@ -0,0 +1,54 @@
+﻿using HarmonyLib;
+using Mono.Cecil;
+
+namespace AirportCEOStaffImprovements.Confirmations;
+
+
+
+[HarmonyPatch(typeof(EmployeeContainerUI))]
+internal class TrainConfirmationPatch
+{
+    static bool SkipDialogCheck = false;
+
+    [HarmonyPatch(nameof(EmployeeContainerUI.TrainEmployee))]
+    [HarmonyPrefix]
+    internal static bool TrainConfirmation_Prefix(EmployeeController employee)
+    { 
+        if(!SIConfig.ShowTrainConfirmation.Value)
+        {
+            return true;
+        }
+
+        if (SkipDialogCheck)
+        {
+            return true;
+        }
+
+        DialogPanel.Instance.ShowQuestionPanel(
+            (result) => TrainEmployee(result, employee),
+            $"Are you sure you want to train {employee.employeeModel.FullName}?",
+            true
+        );
+
+
+        return false;
+    }
+
+    private static void TrainEmployee(bool result, EmployeeController employee)
+    {
+        if (!result)
+        {
+            return;
+        }
+
+        SkipDialogCheck = true;
+        var employeePanelUI = Singleton<EmployeePanelUI>.Instance;
+        Singleton<EmployeeContainerUI>.Instance.TrainEmployee(employee, () => employeePanelUI.GenerateEmployeeContainers());
+        SkipDialogCheck = false;
+
+        if (employeePanelUI)
+        {
+            employeePanelUI.GenerateEmployeeContainers();
+        }
+    }
+}
--- SIConfig.cs
+++ SIConfig.cs
@@ -1,10 +1,5 @@
 ﻿using AirportCEOStaffImprovements.SortingEmployees.Models;
 using BepInEx.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AirportCEOStaffImprovements;
 
@@ -23,21 +18,33 @@ internal class SIConfig
 
     internal static ConfigEntry<bool> AllowStaffToBeFullyTrained { get; private set; }
 
+    internal static ConfigEntry<bool> ShowHireConfirmation { get; private set; }
+    internal static ConfigEntry<bool> ShowFireConfirmation { get; private set; }
+    internal static ConfigEntry<bool> ShowTrainConfirmation { get; private set; }
+    internal static ConfigEntry<bool> ShowRejectConfirmation { get; private set; }
+
 
     internal static void SetUpConfig()
     {
         UseTrainAllButton = AirportCEOStaffImprovements.ConfigReference.Bind("General", "Create Train All Button", true, "Create a button to train all staff with.");
-        StaffRespawnSpeed = AirportCEOStaffImprovements.ConfigReference.Bind("General Applicants", "Staff Respawn Speed", 1f, 
+
+        AllowStaffToBeFullyTrained = AirportCEOStaffImprovements.ConfigReference.Bind("General", "Allow Staff To Be Fully Trained", true, "Allow staff to be fully trained instead of only 3 times");
+
+        StaffRespawnSpeed = AirportCEOStaffImprovements.ConfigReference.Bind("General Applicants", "Staff Respawn Speed", 1f,
             new ConfigDescription("Around how fast should staff respawn?", new AcceptableValueRange<float>(0.5f, 5f)));
-        MaxStaffGenerated = AirportCEOStaffImprovements.ConfigReference.Bind("General Applicants", "Max Staff Generated", 8, 
+        MaxStaffGenerated = AirportCEOStaffImprovements.ConfigReference.Bind("General Applicants", "Max Staff Generated", 8,
             new ConfigDescription("The max number of staff generated for hiring at a given time, per type", new AcceptableValueRange<int>(8, 16)));
 
-        SyncStaffFilters = AirportCEOStaffImprovements.ConfigReference.Bind("General Filters", "Sync Staff Filters", true, "Sync the staff filters between the staff and applicants screens");  
+        SyncStaffFilters = AirportCEOStaffImprovements.ConfigReference.Bind("General Filters", "Sync Staff Filters", true, "Sync the staff filters between the staff and applicants screens");
 
         SortByEmployeeType = AirportCEOStaffImprovements.ConfigReference.Bind("General Sorting", "Sort By Employee Type", true, "Sort staff and applicants by their employee type");
-        SortOptions = AirportCEOStaffImprovements.ConfigReference.Bind("General Sorting", "Sort Options",  SortByEnum.Skill, "Sort staff and applicants by skill when hiring");
+        SortOptions = AirportCEOStaffImprovements.ConfigReference.Bind("General Sorting", "Sort Options", SortByEnum.Skill, "Sort staff and applicants by skill when hiring");
         SortDirection = AirportCEOStaffImprovements.ConfigReference.Bind("General Sorting", "Sort Direction", SortDirectionEnum.Descending, "Ascending means low to hight, Descending means high to low");
 
-        AllowStaffToBeFullyTrained = AirportCEOStaffImprovements.ConfigReference.Bind("General Training", "Allow Staff To Be Fully Trained", true, "Allow staff to be fully trained instead of only 3 times");
-    }   
+
+        ShowHireConfirmation = AirportCEOStaffImprovements.ConfigReference.Bind("General Confirmations", "Show Hire Confirmation", false, "Show a confirmation dialog when hiring an employee");
+        ShowFireConfirmation = AirportCEOStaffImprovements.ConfigReference.Bind("General Confirmations", "Show Fire Confirmation", true, "Show a confirmation dialog when firing an employee");
+        ShowTrainConfirmation = AirportCEOStaffImprovements.ConfigReference.Bind("General Confirmations", "Show Train Confirmation", false, "Show a confirmation dialog when training an employee");
+        ShowRejectConfirmation = AirportCEOStaffImprovements.ConfigReference.Bind("General Confirmations", "Show Reject Confirmation", true, "Show a confirmation dialog when rejecting an employee");
+    }
 }
